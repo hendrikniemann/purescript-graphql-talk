f767714730df85d37bb9ca0ad74236418105931d
hendrikniemann <hendrik.niemann@kiron.ngo>
2019-03-15 18:07:53 +0100

    Create static version of the talk for GH pages

---
+++ precache-manifest.3ac805d1c240c7d88f934335db66741f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7a925c7c07f163f3274df0c1ab2c321",
+    "url": "/static/media/twitter.a7a925c7.svg"
+  },
+  {
+    "revision": "0a12fd0e2654ea030ceaab7e245dd415",
+    "url": "/static/media/profile.0a12fd0e.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f7f637f6f4a8df990a97",
+    "url": "/static/js/main.f7f637f6.chunk.js"
+  },
+  {
+    "revision": "c133dc231a3b21ea4082",
+    "url": "/static/js/1.c133dc23.chunk.js"
+  },
+  {
+    "revision": "f7f637f6f4a8df990a97",
+    "url": "/static/css/main.115e12c7.chunk.css"
+  },
+  {
+    "revision": "c133dc231a3b21ea4082",
+    "url": "/static/css/1.3de56c85.chunk.css"
+  },
+  {
+    "revision": "48ccf82ee755604ec319dc3df4e904a5",
+    "url": "/index.html"
+  }
+];
---
+++ precache-manifest.6520289a50b52e8cf9b147815f3ffec5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7a925c7c07f163f3274df0c1ab2c321",
+    "url": "/purescript-graphql-talk/static/media/twitter.a7a925c7.svg"
+  },
+  {
+    "revision": "0a12fd0e2654ea030ceaab7e245dd415",
+    "url": "/purescript-graphql-talk/static/media/profile.0a12fd0e.jpg"
+  },
+  {
+    "revision": "67725ee335edce117cc2",
+    "url": "/purescript-graphql-talk/static/js/runtime~main.67725ee3.js"
+  },
+  {
+    "revision": "f7f637f6f4a8df990a97",
+    "url": "/purescript-graphql-talk/static/js/main.f7f637f6.chunk.js"
+  },
+  {
+    "revision": "f177edcf98de92f0883a",
+    "url": "/purescript-graphql-talk/static/js/1.f177edcf.chunk.js"
+  },
+  {
+    "revision": "f7f637f6f4a8df990a97",
+    "url": "/purescript-graphql-talk/static/css/main.115e12c7.chunk.css"
+  },
+  {
+    "revision": "f177edcf98de92f0883a",
+    "url": "/purescript-graphql-talk/static/css/1.3de56c85.chunk.css"
+  },
+  {
+    "revision": "0da204196c5e9bce8b95bbf539afec5a",
+    "url": "/purescript-graphql-talk/index.html"
+  }
+];
